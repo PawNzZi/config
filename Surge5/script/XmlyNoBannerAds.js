a1c21cdf4add73e6c2b69d6dc3cc27037b3d619a
PawNzZi <roy20fwmeng@gmail.com>
2023-02-10 10:50:01 +0800

    Update XmlyNoBannerAds.js

--- Surge5/script/XmlyNoBannerAds.js
+++ Surge5/script/XmlyNoBannerAds.js
@@ -3,6 +3,5 @@ var resultData = app.header[0].item.list[0].data;
 var array = resultData.filter(function(item){
   return item.isAd == false;
 })
-console.log(array);
 app.header[0].item.list[0].data = array;
 $done({body:JSON.stringify(app)});
